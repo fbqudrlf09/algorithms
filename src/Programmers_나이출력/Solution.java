cce1c44b34e8012876ea98782bf339cbef914098
ryu <fbqudrlf09@naver.com>
2024-10-24 08:52:24 +0900

    나이 출력

---
+++ src/Programmers_나이출력/Solution.java
@@ -0,0 +1,13 @@
+package Programmers_나이출력;
+
+public class Solution {
+
+    public int solution(int age) {
+        int answer = 0;
+        int nowYear = 2022;
+
+        answer = nowYear - age + 1;
+
+        return answer;
+    }
+}
