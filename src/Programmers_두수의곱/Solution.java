c09fea00fc3dcd23ebe69dcf0a24ac3241637e17
ryu <fbqudrlf09@naver.com>
2024-10-23 09:06:21 +0900

    두수의 곱, 몫 나누기 추가

---
+++ src/Programmers_두수의곱/Solution.java
@@ -0,0 +1,11 @@
+package Programmers_두수의곱;
+
+public class Solution {
+
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        answer = num1 * num2;
+
+        return answer;
+    }
+}
--- src/Programmers_두수의차/Solution.java
+++ src/Programmers_두수의차/Solution.java
@@ -1,5 +1,8 @@
 package Programmers_두수의차;
 
+import java.util.Collections;
+import java.util.LinkedList;
+
 public class Solution {
 
     public static void main(String[] args) {
@@ -13,6 +16,7 @@ public class Solution {
     public int solution(int num1, int num2){
         int answer = 0;
         answer = num1 - num2;
+
 
         return answer;
     }
