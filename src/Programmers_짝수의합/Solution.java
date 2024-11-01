db077cf876c6c6af99950a2c7236ab1b6c3738e6
fbqudrlf09 <fbqudrlf09@naver.com>
2024-11-01 13:26:16 +0900

    짝수의 합'

---
+++ src/Programmers_짝수의합/Solution.java
@@ -0,0 +1,12 @@
+package Programmers_짝수의합;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 2; i <= n; i = i + 2){
+            answer = answer + i;
+        }
+
+        return answer;
+    }
+}
