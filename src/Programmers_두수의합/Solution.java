568aff5e97252aa9d6f2991951a55741189b3d05
fbqudrlf09 <fbqudrlf09@naver.com>
2024-11-01 12:29:36 +0900

    두수의 합

---
+++ src/Programmers_두수의합/Solution.java
@@ -0,0 +1,10 @@
+package Programmers_두수의합;
+
+public class Solution {
+
+        public int solution(int num1, int num2) {
+            int answer = -1;
+            answer = num1 + num2;
+            return answer;
+        }
+}
