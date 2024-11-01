2744825689085b8356aaae5dd42fd966e1d928fd
fbqudrlf09 <fbqudrlf09@naver.com>
2024-11-01 12:36:08 +0900

    두수의 나눗셈

---
+++ src/Programmers_두수의나눗셈/Solution.java
@@ -0,0 +1,15 @@
+package Programmers_두수의나눗셈;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int solution(int num1, int num2) {
+        double value = (double)num1 / num2;
+        int answer = (int)(value * 1000);
+
+        return answer;
+    }
+}
